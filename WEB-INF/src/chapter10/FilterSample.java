93eedb5c19d1f7bd52eaec2b2167de499c5eefbc
yapiko5505 <yapiko5505@users.noreply.github.com>
2023-03-05 13:41:35 +0900

    javadonuts1

---
+++ WEB-INF/src/chapter10/FilterSample.java
@@ -0,0 +1,17 @@
+package chapter10;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(urlPatterns={"/chapter10/filter-sample"})
+public class FilterSample extends HttpServlet {
+
+    public void doGet (
+        HttpServletRequest request, HttpServletResponse response
+    ) throws ServletException, IOException {
+        System.out.println("サーブレットの処理");
+    }
+    
+}
